6adc14fd5d4e55e9a5617497cc214195f1e1228a
arojas <anyara@arionkoder.com>
2026-04-29 01:23:49 -0400

    Add list reporter

--- LoginPageTests/playwright.config.ts
+++ LoginPageTests/playwright.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html']
+  ],
   use: {
     baseURL: Env.baseURL,
     trace: 'on-first-retry',
